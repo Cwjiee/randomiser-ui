5bf5904d5b5c279bdf7f5d4da26cd67a051a7000
Wei Jie <jackchong398@gmail.com>
2023-07-17 22:37:38 +0800

    feat: fetch messages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { onMount } from 'svelte';
+
+  let items = []
+
+  const url = 'http://localhost:3000/messages';
+  onMount( async () => {
+    const res = await fetch(url);
+    items = await res.json();
+  })
+</script>
+
+<h1>Messages</h1>
+
+{#each items as item}
+    <h3>user : {item.user_id}</h3>
+    <h3>item : {item.content}</h3>
+{/each}
