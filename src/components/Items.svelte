e63562ac8264e7665ee41759b23f073717a5a541
Wei Jie <jackchong398@gmail.com>
2023-07-22 18:24:03 +0800

    feat: establish websockets connection

--- src/components/Items.svelte
+++ src/components/Items.svelte
@@ -2,53 +2,16 @@
   import { onMount } from "svelte";
   import { Avatar } from "@skeletonlabs/skeleton";
 	import Users from "./Users.svelte";
+  import {connect} from "../lib/stores";
 
   let items = [];
 
   onMount( async () => {
     const res = await fetch('http://localhost:3000/messages');
     items = await res.json();
+    connect();
   })
 
-  onMount( () => {
-    const socket = new WebSocket('ws://localhost:3000/cable');
-    socket.onopen = () => {
-      console.log('connected');
-
-      const message = {
-        command: 'subscribe',
-        identifier: JSON.stringify({
-          channel: 'MessagesChannel'
-        }),
-      };
-
-      if(message.type === 'ping') {
-        return;
-      }
-      socket.send(JSON.stringify(message));
-    };
-
-    socket.onmessage = (event) => {
-      const message = JSON.parse(event.data);
-
-      if(message.type === "ping") {
-        return;
-      }
-
-      console.log('message received : ' + message.identifier);
-      if(message.content){
-        items = [...items, message.content];
-      }
-    };
-
-    socket.onclose = () => {
-      console.log('websocket disconnected');
-    };
-
-    socket.onerror = (error) => {
-      console.log('websocket error:' + error);
-    }
-  });
 </script>
 
  {#each items as item}
@@ -63,4 +26,11 @@
      </div>
    </div>
  {/each}
+
+<!-- <ul class="list">
+	<li>
+		<span class="flex-auto"></span>
+	</li>
+</ul>
+ -->
 
--- src/lib/stores.js
+++ src/lib/stores.js
@@ -2,3 +2,40 @@
 import { writable } from "svelte/store";
 
 export const currentUser = writable({});
+export const msg = writable({content: ''});
+
+export const connect = async () => {
+  const socket = new WebSocket('ws://localhost:3000/cable');
+  socket.addEventListener('open', () => {
+    console.log('socket open');
+
+    socket.send(JSON.stringify({
+      command: "subscribe",
+      identifier: JSON.stringify({
+        channel: "MessagesChannel"
+      })
+    }));
+  });
+  
+  socket.addEventListener('message', (event) => {
+    const data = JSON.parse(event.data);
+    if(data.type === 'ping') return;
+    console.log(data);
+
+    if(data.type === 'confirm_subscription') {
+      if(data.message) {
+        msg.set(({content: data.message.content}));
+      }
+    }
+    
+    msg.set(({content: data.content}));
+  });
+  
+  socket.addEventListener('close', () => {
+    console.log('socket closed');
+  });
+  
+  socket.addEventListener('error', (error) => {
+    console.log('socket error : ' + error);
+  });
+}
