090a4ac5b0f38381b7f650909862183b57802ada
Wei Jie <jackchong398@gmail.com>
2023-08-06 23:15:16 +0800

    feat: display system message

--- src/components/Items.svelte
+++ src/components/Items.svelte
@@ -3,9 +3,18 @@
   import { connectCurrentUser, connectMessage } from "../lib/stores";
   import store from "../lib/stores";  
   import { Avatar } from "@skeletonlabs/skeleton";
+	import SystemMessage from "./SystemMessage.svelte";
 
   let items = [];
   let users = [];
+  let systemMessage = [];
+  let sentOnce = false;
+  
+  const ConicStop = [
+    { color: 'transparent', start: 0, end: 25 },
+    { color: 'rgb(var(--color-primary-500))', start: 75, end: 100 }
+  ];
+
 
   onMount( async () => {
     const resMessage = await fetch('http://localhost:3000/messages');
@@ -20,6 +29,11 @@
 
     store.subscribeMessage((currentMessage) => {
       if (currentMessage.content !== undefined) {
+        if (currentMessage.system_message && !sentOnce) {
+          systemMessage = [...systemMessage, currentMessage];
+          sentOnce = true;
+          console.log(systemMessage)
+        }
         items = [...items, currentMessage];
         console.log(items)
       }
@@ -36,22 +50,25 @@
 </script>
 
 {#each items as item}
-    <div class="grid grid-cols-[auto_1fr] gap-2">
-      {#each users as user}
-        {#if user}
-          {#if user.id === item.user_id}
-            <Avatar src="" initials="{user.username.split(" ").map((n) => n[0]).join("")}" width="w-12" />
-            <div class="card p-4 variant-soft rounded-tl-none space-y-2">
-              <header class="flex justify-between items-center">
-                    <p class="font-bold">{user.username}</p>
-                <small class="opacity-50">{item.created_at}</small>
-              </header>
-              <p>{item.content}</p>
-            </div>
-          {/if}
+  <div class="grid grid-cols-[auto_1fr] gap-2">
+    {#each users as user}
+      {#if user}
+        {#if user.id === item.user_id}
+          <Avatar src="" initials="{user.username.split(" ").map((n) => n[0]).join("")}" width="w-12" />
+          <div class="card p-4 variant-soft rounded-tl-none space-y-2">
+            <header class="flex justify-between items-center">
+                <p class="font-bold">{user.username}</p>
+              <small class="opacity-50">{item.created_at}</small>
+            </header>
+            <p>{item.content}</p>
+          </div>
         {/if}
-      {/each}
-    </div>
+      {/if}
+    {/each}
+  </div>
+{/each}
+{#each systemMessage as message }
+  <SystemMessage message={message} />
 {/each}
 
 
---
+++ src/components/SystemMessage.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { ConicGradient } from "@skeletonlabs/skeleton";
+	import { onMount } from "svelte";
+
+  export let message;
+  let loading;
+  
+  const conicStops = [
+    { color: 'transparent', start: 0, end: 25 },
+    { color: 'rgb(var(--color-primary-500))', start: 75, end: 100 }
+  ];
+
+  onMount(async () => {
+    loading = true;
+    setTimeout(() => {
+      loading = false;
+    }, 1000);
+  })
+
+</script>
+
+{#if loading}
+  <ConicGradient stops={conicStops} spin>Loading</ConicGradient>
+{:else}
+  <div class="card p-4 variant-soft space-y-2">
+    <header class="flex justify-between items-center">
+      <p class="font-bold">System result</p>
+    </header>
+    {#each message.content as content}
+      <p>{content}</p>
+    {/each}
+  </div>
+{/if}
